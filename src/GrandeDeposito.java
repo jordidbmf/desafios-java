e69542436465bd212ba2dc879420043a3a71c0da
jordidbmf <jordi@zamplex.com.br>
2023-09-05 11:03:29 -0300

    Desafio grande deposito

---
+++ src/GrandeDeposito.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GrandeDeposito {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double valor = scanner.nextDouble();
+
+        if (valor > 0){
+            System.out.println("Deposito realizado com sucesso!");
+            System.out.println("Saldo atual: R$ " + String.format("%.2f", valor));
+        } else if (valor < 0){
+            System.out.println("Valor invalido! Digite um valor maior que zero.");
+        } else {
+            System.out.println("Encerrando o programa...");
+        }
+    }
+}
